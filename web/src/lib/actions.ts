11dd74f0ee0f9c069e63eb46433d378b88a93840
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-04-09 14:34:54 -0300

    fix: added missing fix event

--- web/src/lib/actions.ts
+++ web/src/lib/actions.ts
@@ -9,8 +9,11 @@ export default function getActions(): Action[] {
         { label: 'Respawn', action: 'menu:respawn', hideUI: true },
         { label: 'Heal', action: 'menu:heal', hideUI: false },
         { label: 'Invincible', action: 'menu:invincible', hideUI: true },
-        { label: 'Remove Wanted', action: 'menu:removeWanted', hideUI: true },
+        { label: 'Add Wanted Level', action: 'menu:addWantedLevel', hideUI: false },
+        { label: 'Remove Wanted Level', action: 'menu:removeWantedLevel', hideUI: false },
+        { label: 'Clear Wanted Level', action: 'menu:clearWantedLevel', hideUI: false },
         { label: 'Resurrect Ped', action: 'menu:resurrectPedNetwork', hideUI: true },
+        { label: 'Fix Vehicle', action: 'menu:fixVehicle', hideUI: false}
     ];
 
     return actions;
