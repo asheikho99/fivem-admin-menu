4c2ce956c7cd2ab3b2855fd156932102242cd105
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-04-09 14:02:54 -0300

    Prettier svelte support

--- client/client.ts
+++ client/client.ts
@@ -1,7 +1,7 @@
 console.log('Hello from client.ts');
 
 RegisterCommand(
-    'svelte:show',
+    'menu',
     () => {
         SendNUIMessage({
             action: 'setVisible',
@@ -25,4 +25,41 @@ RegisterNuiCallback('getClientData', (data: any, cb: ({}) => void) => {
 RegisterNuiCallback('hideUI', (data: any, cb: ({}) => void) => {
     cb({});
     SetNuiFocus(false, false);
-})
+});
+
+RegisterNuiCallback('menu:respawn', (data: any, cb: ({}) => void) => {
+    SetEntityHealth(PlayerPedId(), 0);
+    cb({});
+});
+
+RegisterNuiCallback('menu:heal', (data: any, cb: ({}) => void) => {
+    SetEntityHealth(PlayerPedId(), 200);
+    cb({});
+});
+
+RegisterNuiCallback('menu:invincible', (data: any, cb: ({}) => void) => {
+    const playerPed = PlayerPedId();
+    const invincible = GetPlayerInvincible(PlayerId());
+
+    SetEntityInvincible(playerPed, !invincible);
+    SetPedCanRagdoll(playerPed, !invincible);
+    SetEntityAlpha(playerPed, invincible ? 255 : 153, false);
+
+});
+
+RegisterNuiCallback('menu:removeWanted', (data: any, cb: ({}) => void) => {
+    SetPlayerWantedLevel(PlayerId(), 0, false);
+    cb({});
+});
+
+RegisterNuiCallback('menu:fixVehicle', (data: any, cb: ({}) => void) => {
+    const vehicle = GetVehiclePedIsIn(PlayerPedId(), false);
+    SetVehicleFixed(vehicle);
+    cb({});
+});
+
+RegisterNuiCallback('menu:respawnPed', (data: any, cb: ({}) => void) => {
+    const [x, y, z] = GetEntityCoords(PlayerPedId(), true);
+    NetworkResurrectLocalPlayer(x, y, z, 0, true, false);
+    cb({});
+});
